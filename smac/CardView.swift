be3bb291048f8ec289b0c979a560077d6514a042
Sean Velasco <sean@sean.app>
2024-09-12 15:46:47 +0800

    Added Perks-related Views

---
+++ smac/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  smac
+//
+//  Created by Sean Velasco on 9/12/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        NavigationStack {
+            
+        }
+    }
+}
+
+#Preview {
+    CardView()
+}
--- smac/HomeView.swift
+++ smac/HomeView.swift
@@ -21,7 +21,7 @@ struct CardCarouselView: View {
         }
         .scrollTargetBehavior(.viewAligned)
         .contentMargins(34, for: .scrollContent)
-//        .contentMargins(.horizontal, 34)
+        //        .contentMargins(.horizontal, 34)
     }
 }
 
@@ -37,7 +37,7 @@ struct PointsPreview: View {
                     }
                 }
             }
-            .padding([.top, .bottom])
+            .padding(.vertical)
             .frame(maxWidth: .infinity)
         }
     }
@@ -52,6 +52,110 @@ struct CardPreviewSheetView: View {
             Text("Card number")
         } footer: {
             Text("Valid Until")
+        }
+    }
+}
+
+struct ShortcutView: View {
+    var image: String
+    var text: String
+    var body: some View {
+        Button {
+            
+        } label: {
+            VStack {
+                Image(systemName: image)
+                    .padding()
+                    .background(.tertiary)
+                    .clipShape(.circle)
+                Text(text)
+                    .font(.subheadline)
+                    .foregroundColor(.primary)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+                    .frame(maxWidth: 65)
+            }
+        }
+    }
+}
+
+struct ShortcutsViwe: View {
+    var body: some View {
+        HStack(alignment: .top) {
+            Spacer()
+            ShortcutView(image: "cart", text: "Shop")
+            Spacer()
+            ShortcutView(image: "tag", text: "Vouchers")
+            Spacer()
+            ShortcutView(image: "arrow.up.arrow.down", text: "Transfer")
+            Spacer()
+            ShortcutView(image: "clock", text: "History")
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
+
+struct PerkCardView: View {
+    let imageUrl = URL(string: "")
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            AsyncImage(url: imageUrl) { image in
+                image
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fill)
+            } placeholder: {
+                EmptyView()
+            }
+            VStack(alignment: .leading) {
+                Text("10% Off".uppercased())
+                    .bold()
+                Text("Until Octover 31")
+                    .font(.caption)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .frame(width: 165, height: 225, alignment: .bottom)
+        .overlay(
+            RoundedRectangle(cornerRadius: 12)
+                .strokeBorder(.separator, lineWidth: 1)
+        )
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+    }
+}
+
+struct PerksPreviewView: View {
+    //    let columns: [GridItem] = [
+    //        GridItem(.flexible(), spacing: 12),
+    //        GridItem(.flexible(), spacing: 12)
+    //    ]
+    var body: some View {
+        Section {
+            ForEach(0..<3) { perks in
+                Section {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHStack(spacing: 12) {
+                            ForEach(0..<10, id: \.self) { character in
+                                PerkCardView()
+                            }
+                        }
+                        .scrollTargetLayout()
+                    }
+                    .scrollTargetBehavior(.viewAligned)
+                    .contentMargins(24, for: .scrollContent)
+                } header: {
+                    Text("Food Perks")
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal)
+                }
+            }
+        } header: {
+            Text("Members Only Perks")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .padding(.horizontal)
         }
     }
 }
@@ -78,6 +182,13 @@ struct HomeView: View {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
                     CardCarouselView(cards: cards)
+                    Divider()
+                    Group {
+                        ShortcutsViwe()
+                            .padding()
+                        PerksPreviewView()
+                            .padding(.bottom)
+                    }
                 }
             }
             .toolbarBackground(.visible, for: .navigationBar)
@@ -100,12 +211,12 @@ struct HomeView: View {
             }
         }
         .sheet(isPresented: $displayPointsPreview) {
-//            PointsPreview(cards: cards)
+            //            PointsPreview(cards: cards)
             CardPreviewSheetView(card: cards[0])
                 .presentationDetents([.height(700)])
-//                .presentationCornerRadius(0)
+            //                .presentationCornerRadius(0)
                 .presentationDragIndicator(.visible)
-//                .interactiveDismissDisabled(true)
+            //                .interactiveDismissDisabled(true)
         }
     }
 }
